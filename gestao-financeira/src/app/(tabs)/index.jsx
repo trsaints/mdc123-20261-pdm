58405f74d093703e73133c38afd02d278a84d8d3
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2026-05-27 23:47:02 +0000

    update: render first transaction on main tab

--- gestao-financeira/src/app/(tabs)/index.jsx
+++ gestao-financeira/src/app/(tabs)/index.jsx
@@ -1,5 +1,10 @@
-import { Text } from "react-native"
+import { useContext } from "react";
+import { Text } from "react-native";
+import { MoneyContext } from "../../../contexts/GlobalState";
 
 export default function Transactions() {
-  return <Text>Transações</Text>
+  const [transactions] = useContext(MoneyContext);
+
+  // Exibe a descrição do primeiro item da lista (posição 0), se existir (?)
+  return <Text>{transactions[0]?.description}</Text>;
 }
