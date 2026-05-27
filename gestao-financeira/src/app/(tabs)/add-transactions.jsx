671dc8cbffc1e6e3ce2a2dbd6e67aeac6d7c0fd3
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2026-05-27 23:25:22 +0000

    feat: enable date period and category selection on 'add-transactions' tab

--- gestao-financeira/src/app/(tabs)/add-transactions.jsx
+++ gestao-financeira/src/app/(tabs)/add-transactions.jsx
@@ -6,40 +6,52 @@ import {
   Alert,
   StyleSheet,
   Button,
+  TouchableOpacity,
+  Platform,
 } from "react-native";
 import { globalStyles } from "../../styles/globalStyles";
 import { useState } from "react";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
+import { Picker } from "@react-native-picker/picker";
+import { categories } from "../../constants/categories";
+import { colors } from "../../constants/colors";
+
 export default function AddTransactions() {
   const initialForm = {
     description: "",
     value: 0,
-    date: "",
+    date: new Date(),
     category: "Renda",
   };
-  // Estado inicial como um objeto
+
   const [form, setForm] = useState(initialForm);
+  const [showPicker, setShowPicker] = useState(false);
 
   const addTransaction = () => {
     Alert.alert(
-      `${form.description} | ${form.value} | ${form.date} | ${form.category}`,
+      `${form.description} | ${form.value} | ${form.date} | ${form.category}`
     );
   };
 
   const handleCurrencyChange = (text) => {
-    // 1. Regex Mágico: Remove tudo o que NÃO for número (letras, R$, vírgulas)
     const formattedValue = text.replace(/\D/g, "");
-
-    // 2. Transforma em número decimal (divide por 100 para criar os centavos)
     const numberValue = formattedValue ? parseFloat(formattedValue) / 100 : 0;
 
-    // 3. Salva no estado
     setForm({ ...form, value: numberValue });
+  };
+
+  const handleDateChange = (_, selectDate) => {
+    setShowPicker(false);
+
+    if (selectDate) {
+      setForm({ ...form, date: selectDate });
+    }
   };
 
   return (
     <View style={globalStyles.screenContainer}>
       <ScrollView style={globalStyles.content}>
-        <View style={StyleSheet.form}>
+        <View style={styles.form}>
           <View>
             <Text style={globalStyles.inputLabel}>Descrição</Text>
             <TextInput
@@ -52,8 +64,11 @@ export default function AddTransactions() {
           <View>
             <Text style={globalStyles.inputLabel}>Valor</Text>
             <TextInput
-              value={form.value}
-              onChangeText={(text) => setForm({ ...form, value: text })}
+              value={form.value.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+              onChangeText={handleCurrencyChange}
               keyboardType="numeric"
               style={globalStyles.input}
             />
@@ -61,20 +76,56 @@ export default function AddTransactions() {
 
           <View>
             <Text style={globalStyles.inputLabel}>Data</Text>
-            <TextInput
-              value={form.date}
-              onChangeText={(text) => setForm({ ...form, date: text })}
-              style={globalStyles.input}
-            />
+            <TouchableOpacity onPress={() => setShowPicker(true)}>
+              <TextInput
+                value={form.date.toLocaleDateString("pt-BR")}
+                onChangeText={(text) => setForm({ ...form, date: text })}
+                style={globalStyles.input}
+                editable={false}
+              />
+            </TouchableOpacity>
+
+            {showPicker && (
+              <RNDateTimePicker
+                mode="date"
+                display={Platform.OS === "ios" ? "inline" : "default"}
+                value={form.date}
+                onChange={handleDateChange}
+              />
+            )}
           </View>
 
           <View>
             <Text style={globalStyles.inputLabel}>Categoria</Text>
-            <TextInput
-              value={form.category}
-              onChangeText={(text) => setForm({ ...form, category: text })}
-              style={globalStyles.input}
-            />
+            <View style={styles.picker}>
+              <Picker
+                selectedValue={form.category}
+                onValueChange={(itemValue) =>
+                  setForm({ ...form, category: itemValue })
+                }
+              >
+                <Picker.Item
+                  label={categories.income.displayName}
+                  value={categories.income.name}
+                />
+                <Picker.Item
+                  label={categories.food.displayName}
+                  value={categories.food.name}
+                />
+                <Picker.Item
+                  label={categories.house.displayName}
+                  value={categories.house.name}
+                />
+                <Picker.Item
+                  label={categories.education.displayName}
+                  value={categories.education.name}
+                />
+                <Picker.Item
+                  label={categories.travel.displayName}
+                  value={categories.travel.name}
+                />
+              </Picker>
+            </View>
           </View>
         </View>
 
@@ -83,3 +134,20 @@ export default function AddTransactions() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    gap: 12,
+    marginBottom: 40,
+    marginTop: 10,
+  },
+  picker: {
+    display: "flex",
+    justifyContent: "center",
+    height: 44,
+    borderColor: colors.secondaryText,
+    borderWidth: 1,
+    borderRadius: 8,
+    flexGrow: 1,
+  },
+});
