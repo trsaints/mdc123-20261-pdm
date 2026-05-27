701400850c68ac015f0c98cba5ac97b5434004c0
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2026-05-27 23:52:38 +0000

    update: render custom list for displaying user transactions when available

--- gestao-financeira/src/app/(tabs)/index.jsx
+++ gestao-financeira/src/app/(tabs)/index.jsx
@@ -1,10 +1,24 @@
+import { MoneyContext } from "@/contexts/GlobalState";
 import { useContext } from "react";
-import { Text } from "react-native";
-import { MoneyContext } from "../../../contexts/GlobalState";
+import { FlatList, Text, View } from "react-native";
+import TransactionItem from "../components/TransactionItem";
+import { globalStyles } from "../../styles/globalStyles";
 
 export default function Transactions() {
   const [transactions] = useContext(MoneyContext);
 
-  // Exibe a descrição do primeiro item da lista (posição 0), se existir (?)
-  return <Text>{transactions[0]?.description}</Text>;
+  return (
+    <View style={globalStyles.screenContainer}>
+      <FlatList
+        data={transactions}
+        renderItem={({ item }) => <TransactionItem {...item} />}
+        ListEmptyComponent={
+          <Text style={globalStyles.secondaryText}>
+            Ainda não há nenhum item!
+          </Text>
+        }
+        style={globalStyles.content}
+      />
+    </View>
+  );
 }
