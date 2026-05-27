1931f95abc40a3635c8fe1b5798a7aa732a833a0
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2026-05-27 23:42:40 +0000

    feat: define context for storing session data in-memory

--- gestao-financeira/src/app/_layout.jsx
+++ gestao-financeira/src/app/_layout.jsx
@@ -1,15 +1,17 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { colors } from "../constants/colors";
+import GlobalState from "../../contexts/GlobalState";
 
 export default function RootLayout() {
   return (
-    <>
+    // Agora todas as telas dentro do Stack tem acesso ao Contexto!
+    <GlobalState>
       <StatusBar backgroundColor={colors.primary} style="light" />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-    </>
+    </GlobalState>
   );
 }
