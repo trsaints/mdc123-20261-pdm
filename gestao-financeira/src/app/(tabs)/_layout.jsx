c3e7067399b505425e2ff3e7644a5de55967f5aa
trsaints <thiago.dev2k3@gmail.com>
2026-05-28 20:16:59 -0300

    update: include new tab on main screen

--- gestao-financeira/src/app/(tabs)/_layout.jsx
+++ gestao-financeira/src/app/(tabs)/_layout.jsx
@@ -4,71 +4,80 @@ import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { colors } from "../../constants/colors";
 
 export default function TabsLayout() {
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarHideOnKeyboard: true,
-        headerStyle: { backgroundColor: colors.primary },
-        headerTintColor: colors.primaryContrast,
-        headerTitleAlign: "center",
-        tabBarActiveTintColor: colors.primary,
-        tabBarInactiveTintColor: colors.inactive,
-        tabBarStyle: {
-          height: 60,
-          paddingTop: 5,
-          backgroundColor: colors.background,
-        },
-        tabBarButton: (props) => (
-          <TouchableOpacity {...props} activeOpacity={0.8} />
-        ),
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Transações",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="attach-money" size={28} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="add-transactions"
-        options={{
-          title: "Adicionar Transação",
-          tabBarLabel: "",
-          tabBarIcon: () => (
-            <View style={styles.addButton}>
-              <MaterialIcons
-                name="add"
-                size={40}
-                color={colors.primaryContrast}
-              />
-            </View>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="summary"
-        options={{
-          title: "Resumo",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="pie-chart" size={28} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarHideOnKeyboard: true,
+				headerStyle: { backgroundColor: colors.primary },
+				headerTintColor: colors.primaryContrast,
+				headerTitleAlign: "center",
+				tabBarActiveTintColor: colors.primary,
+				tabBarInactiveTintColor: colors.inactive,
+				tabBarStyle: {
+					height: 60,
+					paddingTop: 5,
+					backgroundColor: colors.background,
+				},
+				tabBarButton: (props) => (
+					<TouchableOpacity {...props} activeOpacity={0.8} />
+				),
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: "Transações",
+					tabBarIcon: ({ color }) => (
+						<MaterialIcons name="attach-money" size={28} color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="add-transactions"
+				options={{
+					title: "Adicionar Transação",
+					tabBarLabel: "",
+					tabBarIcon: () => (
+						<View style={styles.addButton}>
+							<MaterialIcons
+								name="add"
+								size={40}
+								color={colors.primaryContrast}
+							/>
+						</View>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="categories"
+				options={{
+					title: "Categorias",
+					tabBarIcon: ({ color }) => (
+						<MaterialIcons name="category" size={26} color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="summary"
+				options={{
+					title: "Resumo",
+					tabBarIcon: ({ color }) => (
+						<MaterialIcons name="pie-chart" size={28} color={color} />
+					),
+				}}
+			/>
+		</Tabs>
+	);
 }
 
 const styles = StyleSheet.create({
-  addButton: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    height: 64,
-    width: 64,
-    borderRadius: 32,
-    backgroundColor: colors.primary,
-  },
+	addButton: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		height: 64,
+		width: 64,
+		borderRadius: 32,
+		backgroundColor: colors.primary,
+	},
 });
